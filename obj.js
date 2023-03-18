af86ca6b7ec53a9091c7cbdf83ad35c991b0250e
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-03-18 22:03:29 +0530

    js
    new files

---
+++ obj.js
@@ -0,0 +1,21 @@
+let animal={
+    name:"cow",legs:4
+}
+console.log(animal.name);
+console.log(animal.legs);
+
+// arrays
+let selectbooks=["the good book","make money","good habits"];
+console.log(selectbooks[2])
+
+// functions
+
+function hello(){
+    console.log("hello all");
+}
+hello()
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(10,2))
