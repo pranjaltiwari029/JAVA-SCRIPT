c861b3795695dd646b55dcb7b17ae3428f99ced8
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-03-19 09:34:58 +0530

    js files

---
+++ obj1.js
@@ -0,0 +1,16 @@
+let lecture =10;
+let section =3;
+let title='js';
+
+const course={
+    lecture:10,section:3,title:'js',
+    notes:{intro:"welcome to JS course"},
+    enroll(){
+        console.log("you are successfully enrolled")
+    }
+
+
+}
+course.enroll()
+console.log(course.title)
+
