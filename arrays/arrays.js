41de04bb5bb67cc0e40040f1cd3d34810713649f
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-03-19 22:59:23 +0530

    arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,29 @@
+const array=[1,2,3,4,5,6,7,8,9]
+console.log(array)
+console.log(array[4])
+
+console.log(array[array.length-1])
+
+//push
+array.push(10)
+
+console.log(array)
+//unshift  add at the starting of array
+
+console.log(array.unshift(0))
+
+console.log(array.pop())
+//shift remove element from starting
+array.shift()
+
+array[0]='hello'
+
+console.log(array)
+
+const names=['pranjal','tapan','sanal','akash']
+console.log(names)
+
+console.log(names.indexOf('pranjal'))
+
+//last inndex of
+console.log(names.lastIndexOf('pranjal'))
