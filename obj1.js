210947989701472a0e5884f604304fbf716f1809
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-03-19 09:47:36 +0530

    ..

--- obj1.js
+++ obj1.js
@@ -2,15 +2,38 @@ let lecture =10;
 let section =3;
 let title='js';
 
-const course={
-    lecture:10,section:3,title:'js',
-    notes:{intro:"welcome to JS course"},
-    enroll(){
-        console.log("you are successfully enrolled")
+// const course={
+//     lecture:10,section:3,title:'js',
+//     notes:{intro:"welcome to JS course"},
+//     enroll(){
+//         console.log("you are successfully enrolled")
+//     }
+
+
+// }
+// course.enroll()
+// console.log(course.title)
+
+
+// function createcourse(title){
+//     return{
+//         title:title,
+//         enroll(){
+//             console.log('you are successfully enrolled');
+//         }
+//     }
+// }
+// const course =createcourse();
+// course.enroll()
+// console.log(course)
+
+
+
+function course(title){
+    this.title=title,
+    this.enroll=function(){
+        
+            console.log('you are successfully enrolled');
+        
     }
-
-
 }
-course.enroll()
-console.log(course.title)
-
